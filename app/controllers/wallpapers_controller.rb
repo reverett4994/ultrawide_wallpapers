d6bf2ac8a1f983957ab0be9263872cf7ac7feebc
Ryan Everett <reverett4994@gmail.com>
2017-03-13 04:39:24 -0400

    added img alts and more links

--- app/controllers/wallpapers_controller.rb
+++ app/controllers/wallpapers_controller.rb
@@ -135,7 +135,7 @@ end
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_wallpaper
-      @wallpaper = Wallpaper.find(params[:id])
+      @wallpaper = Wallpaper.where("title LIKE ?",params[:id]).last
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
--- app/models/wallpaper.rb
+++ app/models/wallpaper.rb
@@ -2,8 +2,13 @@ class Wallpaper < ActiveRecord::Base
   has_attached_file :image,styles: {:thumb=> "1000x1000>"},:convert_options => {
                                                             :thumb => "-quality 80 -interlace Plane"
                                                           }
+  validates :title, uniqueness: true
   validates_attachment_content_type :image, content_type: /\Aimage/
   belongs_to :user
   acts_as_votable
   acts_as_taggable # Alias for acts_as_taggable_on :tags
+
+  def to_param
+    "#{title}"
+  end
 end
